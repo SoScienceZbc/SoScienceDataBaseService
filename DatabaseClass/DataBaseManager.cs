7d6e18116d3cf903c827f4a9aa8ddb983fec3c3c
Leo Jensen <lion0005@zbc.dk>
2021-08-11 15:56:15 +0200

    Add Subject

--- DatabaseClass/DataBaseManager.cs
+++ DatabaseClass/DataBaseManager.cs
@@ -603,6 +603,31 @@ namespace SoScienceDataServer
             return id;
         }
         #endregion
+        #region Subject
+        public int AddSubject(string name)
+        {
+            int id = 0;
+            using (MySqlConnection con = new MySqlConnection(this.con))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("SPInsertSubject", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@name", MySqlDbType.VarChar).Value = name;
+                    
+
+                    con.Open();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        id = reader.GetInt32(0);
+                    }
+                    reader.Close();
+                    cmd.Dispose();
+                }
+            }
+            return id;
+        }
+        #endregion
 
     }
 }
